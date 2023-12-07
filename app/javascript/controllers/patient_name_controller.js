85306276ea5963b1f8d0586be3712798c0621943
Tahir-Maqsood <tahir.maqsood@devsinc.com>
2023-12-07 17:48:16 +0500

    Practiced stimulus targets and actions

--- app/javascript/controllers/patient_name_controller.js
+++ app/javascript/controllers/patient_name_controller.js
@@ -2,7 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="patient-name"
 export default class extends Controller {
+  // targets is an array and we can have multiple targets here in single array.
+  // Just like
+  static targets = ["name"];
+
   connect() {
     console.log("Hello I am Connected");
+  }
+
+  getName=()=>{
+    const element= this.nameTarget
+    const name = element.textContent
+    alert(`you clicked on, ${name}`)
   }
 }
